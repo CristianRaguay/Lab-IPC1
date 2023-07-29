15be1cf30c2a7c9189c3640002f2f409d4fd5fcc
CristianRaguay <3045208880114@ingenieria.usac.edu.gt>
2023-07-29 07:58:10 -0600

    Clase 1

---
+++ Clase1_1/src/clase1_1/Clase1_1.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase1_1;
+
+import java.util.Date;
+
+/**
+ *
+ * @author crist
+ */
+public class Clase1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float valor1;
+        valor1 = (float) 11.88388900000;
+        boolean valor2 = false;
+        char caracter = 'f';
+        byte valor3 = 4;
+        System.out.println(valor3);
+        
+        
+        String cadena = "hola mundo";
+        Date fecha = new Date();
+        System.out.println(cadena);
+        System.out.println(fecha);
+    }
+    
+    
+    public static void prueba(){
+        System.out.println("prueba");
+    }
+            
+    
+}
